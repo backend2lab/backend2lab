8dfcaa39ce18514d87100c0b37ead3140d2d2d91
harcop <harcopdrew@gmail.com>
2025-08-27 14:30:13 +0100

    modify test setup

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -8,4 +8,19 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/server',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/test-setup.ts',
+    '!src/main.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
